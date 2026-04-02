64025f3ab7b014ead4c26371a43a4e7160faaf75
Diego Villouta Fredes <diegov17@gmail.com>
2026-04-02 00:04:18 -0300

    Se quitó opción de elegir estado de reserva al ser creada.

--- reservas/admin.py
+++ reservas/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from .models import Cliente, Tecnico, Reserva
 
+class ReservaAdmin(admin.ModelAdmin):
+    # Campos que se muestran en el formulario
+    def get_fields(self, request, obj=None):
+        fields = list(super().get_fields(request, obj))
+        if obj is None:  # Estamos en la página de creación (/add/)
+            if 'estado' in fields:
+                fields.remove('estado')
+        return fields
+
+    # Volvemos a hacer la fecha editable en la creación
+    def get_readonly_fields(self, request, obj=None):
+        return super().get_readonly_fields(request, obj)
+
+    class Media:
+        # Inyectamos JS para manejar el comportamiento dinámico del label
+        js = ('admin/js/reserva_custom.js',)
+
 admin.site.register(Cliente)
 admin.site.register(Tecnico)
-admin.site.register(Reserva)
+admin.site.register(Reserva, ReservaAdmin)
